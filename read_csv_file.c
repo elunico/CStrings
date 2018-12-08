4894edb239b173120d0da38ab36a36af03c8ab48
Thomas Povinelli <tompov227@gmail.com>
2018-12-08 03:27:17 -0500

    Changed example slightly

--- read_csv_file.c
+++ read_csv_file.c
@@ -1,6 +1,6 @@
 /**
  * Example using the strings.c library
- * This program uses function to read in a TSV (tab-separated values) file
+ * This program uses function to read in a CSV (comma-separated values) file
  * and tokenize it according to the tabs along each line
  * It prints out the tokens of each line one line at a time
  * **/
@@ -9,23 +9,32 @@
 #include <errno.h>
 #include <stdio.h>
 
+void usage() {
+  printf("usage: ./readcsv FILE_NAME\n");
+}
+
 int main(int argc, char const **argv) {
-  FILE *f = fopen("rgb.txt", "r");
+  if (!argv[1]) {
+    usage();
+    exit(1);
+  }
+  FILE *f = fopen(argv[1], "r");
   if (f == NULL) {
     strerror(errno);
     abort();
   }
   string_t *t;
   while ((t = readline(f)) != NULL) {
-    string_tokens_t *tokens = tokenize(t, '\t');
+    string_tokens_t *tokens = tokenize(t, ',');
     if (tokens->size < 2) {
       tokens_done(tokens);
       strdec(t);
     } else {
+      printf("===============================================\n");
       for (int i = 0; i < tokens->size; i++) {
-        printf("t: %s | ", charp(get_token(tokens, i)));
+        printf("| %s ", charp(get_token(tokens, i)));
       }
-      printf("\n");
+      printf("|\n");
       strdec(t);
       tokens_done(tokens);
     }
