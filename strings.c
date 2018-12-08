a4354979616d4514a259d5a765de78f308c5283c
Thomas Povinelli <tompov227@gmail.com>
2018-12-08 00:53:47 -0500

    Added additional functions

--- strings.c
+++ strings.c
@@ -1,33 +1,23 @@
+#include "strings.h"
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #ifdef TEST
 #include <assert.h>
 #include <time.h>
 #endif
 
-int isWhitespace(char c)
-{
+int isWhitespace(char c) {
   return c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\v';
 }
 
-typedef struct
-{
-  char* data;
-  // 256 bit hash plus null byte
-  char* hash;
-  size_t size;
-  size_t refcnt;
-} string_t;
+char *sha256_hexdigest(char *o) { return strdup("1sd09e09fa090ae7d"); }
 
-char* sha256_hexdigest(char* o)
-{ return strdup("1sd09e09fa090ae7d"); }
-
-string_t* string(char* from)
-{
-  string_t* s = malloc(sizeof(*s));
+string_t *string(char *from) {
+  string_t *s = malloc(sizeof(*s));
   s->data = strdup(from);
   s->hash = sha256_hexdigest(from);
   s->size = strlen(from);
@@ -35,11 +25,9 @@ string_t* string(char* from)
   return s;
 }
 
-void strinc(string_t* s)
-{ s->refcnt++; }
+void strinc(string_t *s) { s->refcnt++; }
 
-void strdec(string_t* s)
-{
+void strdec(string_t *s) {
   s->refcnt--;
   if (!s->refcnt) {
     free(s->data);
@@ -48,16 +36,8 @@ void strdec(string_t* s)
   }
 }
 
-typedef struct
-{
-  string_t* s;
-  unsigned int start;
-  unsigned int end;
-} string_view_t;
-
-string_view_t* view(string_t* s, unsigned int start, unsigned int end)
-{
-  string_view_t* view = malloc(sizeof(*view));
+string_view_t *view(string_t *s, unsigned int start, unsigned int end) {
+  string_view_t *view = malloc(sizeof(*view));
   view->s = s;
   strinc(s);
   view->start = start;
@@ -65,42 +45,38 @@ string_view_t* view(string_t* s, unsigned int start, unsigned int end)
   return view;
 }
 
-string_t* substring(string_t* from, size_t start, size_t end)
-{
-  char* d = from->data;
-  if (start >= from->size || end >= from->size || start < 0 || end < 0 || start > end) {
+string_t *substring(string_t *from, size_t start, size_t end) {
+  char *d = from->data;
+  if (start >= from->size || end > from->size || start < 0 || end < 0 ||
+      start > end) {
     return NULL;
   }
   d += start;
-  char* last = from->data + end;
+  char *last = from->data + end;
   char prev = *last;
   *last = '\0';
-  string_t* n = string(d);
+  string_t *n = string(d);
   *last = prev;
   return n;
 }
 
-int char_at(string_t* s, int idx)
-{
+int char_at(string_t *s, int idx) {
   if (s->size <= idx) {
     return -1;
   }
-  return (int) *(s->data + idx);
+  return (int)*(s->data + idx);
 }
 
-void view_done(string_view_t* view)
-{
+void view_done(string_view_t *view) {
   strdec(view->s);
   free(view);
 }
 
-int string_compare(string_t* a, string_t* b)
-{
+int string_compare(string_t *a, string_t *b) {
   return strcmp(a->data, b->data);
 }
 
-void print_view(string_view_t* view, FILE* f, int nl)
-{
+void print_view(string_view_t *view, FILE *f, int nl) {
   for (int i = view->start; i < view->end; i++) {
     fputc(*(view->s->data + i), f);
   }
@@ -109,25 +85,23 @@ void print_view(string_view_t* view, FILE* f, int nl)
   }
 }
 
-string_t* stringify(string_view_t* view)
-{
-  char* v = view->s->data + view->start;
-  char* e = view->s->data + view->end;
+string_t *stringify(string_view_t *view) {
+  char *v = view->s->data + view->start;
+  char *e = view->s->data + view->end;
   char p = *e;
   *e = '\0';
-  string_t* ret = string(v);
+  string_t *ret = string(v);
   *e = p;
   return ret;
 }
 
-string_t* concat(string_t* a, string_t* b)
-{
-  char* n = malloc((sizeof(char)) * (a->size + b->size));
+string_t *concat(string_t *a, string_t *b) {
+  char *n = malloc((sizeof(char)) * (a->size + b->size));
   strncpy(n, a->data, a->size);
   n += a->size;
   strncpy(n, b->data, b->size);
   n -= a->size;
-  string_t* ret = malloc(sizeof(*ret));
+  string_t *ret = malloc(sizeof(*ret));
   ret->data = n;
   ret->refcnt = 1;
   ret->hash = sha256_hexdigest(n);
@@ -135,8 +109,7 @@ string_t* concat(string_t* a, string_t* b)
   return ret;
 }
 
-int starts_with(string_t* a, string_t* pat)
-{
+int starts_with(string_t *a, string_t *pat) {
   if (pat->size > a->size) {
     return 0;
   }
@@ -148,10 +121,11 @@ int starts_with(string_t* a, string_t* pat)
   return 1;
 }
 
-int ends_with(string_t* a, string_t* pat)
-{
-  if (pat->size > a->size) { return 0; }
-  char* start = a->data + a->size - pat->size;
+int ends_with(string_t *a, string_t *pat) {
+  if (pat->size > a->size) {
+    return 0;
+  }
+  char *start = a->data + a->size - pat->size;
   for (int i = 0; i < pat->size; i++) {
     if (*(start + i) != *(pat->data + i)) {
       return 0;
@@ -160,24 +134,36 @@ int ends_with(string_t* a, string_t* pat)
   return 1;
 }
 
-typedef struct {
-  string_t *tok;
-  int start, end;
-}string_token_t;
-
-typedef struct stt{
-  string_token_t token;
-  struct stt *next;
-  struct stt *prev
-} string_tokens_t;
-
-string_tokens_t* token(string_t *s, char delim)
-{
-
+string_tokens_t *tokenize(string_t *s, char delim) {
+  string_tokens_t *list = NULL;
+  string_tokens_t *ret = NULL;
+  int start = 0;
+  int idx = 0;
+  while (idx < s->size) {
+    while (*(s->data + idx) != delim && idx < s->size) {
+      idx++;
+    }
+    string_t *tok = substring(s, start, idx);
+    idx++;       // discard delim
+    start = idx; // begin substrings from idx
+    strinc(tok);
+    if (list == NULL) {
+      list = calloc(1, sizeof(*list));
+      list->token = tok;
+      ret = list;
+      ret->size++;
+    } else {
+      list->next = calloc(1, sizeof(*list));
+      list = list->next;
+      list->token = tok;
+      ret->size++;
+    }
+    strdec(tok);
+  }
+  return ret;
 }
 
-int rcontains_help(string_t* a, string_t* s, int ai, int si)
-{
+int rcontains_help(string_t *a, string_t *s, int ai, int si) {
   if (si == s->size) {
     return 1;
   }
@@ -191,11 +177,9 @@ int rcontains_help(string_t* a, string_t* s, int ai, int si)
   }
 }
 
-int rcontains(string_t* a, string_t* s)
-{ return rcontains_help(a, s, 0, 0); }
+int rcontains(string_t *a, string_t *s) { return rcontains_help(a, s, 0, 0); }
 
-int contains(string_t* a, string_t* sub)
-{
+int contains(string_t *a, string_t *sub) {
   if (sub->size > a->size) {
     return 0;
   }
@@ -223,8 +207,7 @@ int contains(string_t* a, string_t* sub)
   return 0;
 }
 
-string_t* trim(string_t* original)
-{
+string_t *trim(string_t *original) {
   size_t start, end;
   size_t idx = 0;
   while (idx < original->size - 1 && isWhitespace(*(original->data + idx))) {
@@ -240,6 +223,17 @@ string_t* trim(string_t* original)
   return substring(original, start, end);
 }
 
+string_t *get_token(string_tokens_t *toks, int idx) {
+  if (idx >= toks->size) {
+    return NULL;
+  }
+  string_tokens_t *ptr = toks;
+  while (idx--) {
+    ptr = ptr->next;
+  }
+  return ptr->token;
+}
+
 #ifdef TEST
 int random_bound() {
   srand(time(NULL));
@@ -249,78 +243,132 @@ int random_bound() {
 int random_bound() { return 500; }
 #else
 
-int random_bound()
-{ return 1; }
+int random_bound() { return 1; }
 
 #endif
 
-int main(int argc, char const** argv)
-{
+string_t *readline(FILE *source) {
+  int size = 10;
+  char *buffer = NULL;
+  int c;
+  int idx = 0;
+  c = getc(source);
+  while (c != '\n' && c != EOF) {
+    if (buffer == NULL) {
+      buffer = malloc((size_t)size);
+    }
+    *(buffer + idx) = (char)c;
+    idx++;
+    if (idx == size) {
+      size = (int)(size * 1.5);
+      if (size < 0) {
+        return NULL;
+      }
+      buffer = realloc(buffer, (size_t)size);
+      if (buffer == NULL) {
+        return NULL;
+      }
+    }
+    c = getc(source);
+  }
+  *(buffer + idx + 1) = '\0';
+  return buffer ? string(buffer) : NULL;
+}
+
+void tokens_done(string_tokens_t *tokens) {
+  if (tokens == NULL) {
+    return;
+  } else {
+    tokens_done(tokens->next);
+    free(tokens->next);
+    strdec(tokens->token);
+  }
+}
+
+char *charp(string_t *s) { return s->data; }
+
+int main(int argc, char const **argv) {
   // for (int i = 0; i < random_bound(); i++) {
-  string_t* s = string("Hello world!   ");
-  string_t* r = string("   hi there! ");
-  string_t* h = substring(s, 0, 5);
-  string_t* g = string("Where in the world is carmen sandiego?");
-  string_t* w = string("       ");
-  string_t* world1 = substring(g, 13, 18);
-  string_t* world2 = substring(s, 6, 11);
-  int eq = string_compare(world1, world2);
-  string_view_t* v1 = view(s, 0, 5);
-  string_view_t* v2 = view(s, 6, 11);
-  string_view_t* v3 = view(s, 5, 7);
+  //  string_t* s = string("Hello world!   ");
+  //  string_t* r = string("   hi there! ");
+  //  string_t* h = substring(s, 0, 5);
+  //  string_t* g = string("Where in the world is carmen sandiego?");
 
-  string_t* c1 = concat(s, g);
-  string_t* c2 = concat(world1, world2);
+  FILE *f = fopen("/home/thomas/Desktop/cstring/colors.csv", "r");
+  if (f == NULL) {
+    strerror(errno);
+    abort();
+  }
+  string_t *t;
+  while ((t = readline(f)) != NULL) {
+    string_tokens_t *tokens = tokenize(t, ',');
+    for (int i = 0; i < tokens->size; i++) {
+      printf("t: %s |", charp(get_token(tokens, i)));
+    }
+    strdec(t);
+    tokens_done(tokens);
+  }
 
-  int n1 = rcontains(s, h);
-  assert(n1 == 1);
-  int n2 = rcontains(s, world1);
-  assert(n2 == 1);
-  int n3 = rcontains(s, world2);
-  assert(n3 == 1);
-  int n4 = rcontains(s, g);
-  assert(n4 == 0);
-  int n5 = rcontains(s, w);
-  assert(n5 == 0);
-  int n6 = rcontains(g, world2);
-  assert(n6 == 1);
-  int n7 = rcontains(world2, g);
-  assert(n7 == 0);
+  // string_t* w = string("       ");
+  // string_t* world1 = substring(g, 13, 18);
+  // string_t* world2 = substring(s, 6, 11);
+  // int eq = string_compare(world1, world2);
+  // string_view_t* v1 = view(s, 0, 5);
+  // string_view_t* v2 = view(s, 6, 11);
+  // string_view_t* v3 = view(s, 5, 7);
 
-  string_t* t1 = trim(w);
-  string_t* t2 = trim(s);
-  string_t* t3 = trim(r);
+  // string_t* c1 = concat(s, g);
+  // string_t* c2 = concat(world1, world2);
 
-  int y1 = starts_with(s, h);
-  int n8 = starts_with(s, world1);
+  // int n1 = rcontains(s, h);
+  // assert(n1 == 1);
+  // int n2 = rcontains(s, world1);
+  // assert(n2 == 1);
+  // int n3 = rcontains(s, world2);
+  // assert(n3 == 1);
+  // int n4 = rcontains(s, g);
+  // assert(n4 == 0);
+  // int n5 = rcontains(s, w);
+  // assert(n5 == 0);
+  // int n6 = rcontains(g, world2);
+  // assert(n6 == 1);
+  // int n7 = rcontains(world2, g);
+  // assert(n7 == 0);
 
-  // print_view(v1, stdout, 1);
-  // print_view(v2, stdout, 1);
-  // print_view(v3, stdout, 1);
+  // string_t* t1 = trim(w);
+  // string_t* t2 = trim(s);
+  // string_t* t3 = trim(r);
 
-  string_t* s1 = stringify(v1);
-  string_t* s2 = stringify(v2);
-  string_t* s3 = stringify(v3);
+  // int y1 = starts_with(s, h);
+  // int n8 = starts_with(s, world1);
 
-  view_done(v1);
-  view_done(v2);
-  view_done(v3);
+  // // print_view(v1, stdout, 1);
+  // // print_view(v2, stdout, 1);
+  // // print_view(v3, stdout, 1);
 
-  char x = (char) char_at(h, 2);
-  strdec(s);
-  strdec(h);
-  strdec(r);
-  strdec(w);
-  strdec(g);
-  strdec(world1);
-  strdec(world2);
-  strdec(s1);
-  strdec(s2);
-  strdec(s3);
-  strdec(c1);
-  strdec(c2);
-  strdec(t1);
-  strdec(t2);
-  strdec(t3);
+  // string_t* s1 = stringify(v1);
+  // string_t* s2 = stringify(v2);
+  // string_t* s3 = stringify(v3);
+
+  // view_done(v1);
+  // view_done(v2);
+  // view_done(v3);
+
+  // char x = (char) char_at(h, 2);
+  // strdec(s);
+  // strdec(h);
+  // strdec(r);
+  // strdec(w);
+  // strdec(g);
+  // strdec(world1);
+  // strdec(world2);
+  // strdec(s1);
+  // strdec(s2);
+  // strdec(s3);
+  // strdec(c1);
+  // strdec(c2);
+  // strdec(t1);
+  // strdec(t2);
+  // strdec(t3);
   // }
 }
--- strings.h
+++ strings.h
@@ -1,3 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifndef STRINGS_H
+#define STRINGS_H
+
+typedef struct
+{
+  char *data;
+  // 256 bit hash plus null byte
+  char *hash;
+  size_t size;
+  size_t refcnt;
+} string_t;
+
+typedef struct
+{
+  string_t *s;
+  unsigned int start;
+  unsigned int end;
+} string_view_t;
+
+typedef struct stt
+{
+  string_t *token;
+  struct stt *next;
+  int size;
+} string_tokens_t;
+
+string_t *string(char *from);
+void strinc(string_t *s);
+void strdec(string_t *s);
+int char_at(string_t *s, int idx);
+string_view_t *view(string_t *s, unsigned int start, unsigned int end);
+void view_done(string_view_t *view);
+string_t *stringify(string_view_t *view);
+void print_view(string_view_t *view, FILE *f, int nl);
+string_t *substring(string_t *from, size_t start, size_t end);
+int string_compare(string_t *a, string_t *b);
+int starts_with(string_t *a, string_t *pat);
+int ends_with(string_t *a, string_t *pat);
+int rcontains(string_t *a, string_t *s);
+int contains(string_t *a, string_t *sub);
+string_t *concat(string_t *a, string_t *b);
+string_t *trim(string_t *original);
+string_tokens_t *tokenize(string_t *s, char delim);
+void tokens_done(string_tokens_t *tokens);
+string_t *get_token(string_tokens_t *toks, int idx);
+string_t *readline(FILE *source);
+char *charp(string_t *s) { return s->data; }
+
+#endif
